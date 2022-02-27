82fe9b6c9831844450e2da5a64ecfde5b7fe5516
matteocourthoud <matteo.courthoud@uzh.ch>
2022-02-27 20:52:07 +0100

    unsupervised

--- notebooks/utils/lecture10.py
+++ notebooks/utils/lecture10.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import time
+from scipy.stats import multivariate_normal
 
 # Sklean
 from sklearn.preprocessing import scale
@@ -17,6 +18,7 @@ from scipy.cluster.hierarchy import linkage, dendrogram, cut_tree
 
 # Import matplotlib for graphs
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 from mpl_toolkits.mplot3d import axes3d
 from IPython.display import clear_output
 
@@ -33,7 +35,7 @@ plt.rcParams['legend.fontsize'] = 14
 def make_figure_10_1a(df_dim2, df_weights):
 
     # Init
-    fig, ax1 = plt.subplots(figsize=(10,10))
+    fig, ax1 = plt.subplots(figsize=(8,8))
     ax1.set_title('Figure 10.1');
 
     # Plot Principal Components 1 and 2
@@ -108,14 +110,115 @@ def make_figure_10_2(pca4):
     ax2.plot([1,2,3,4], np.cumsum(pca4.explained_variance_ratio_))
     ax2.set_ylabel('Cumulative Variance Explained');
     ax2.set_xlabel('Principal Component');
+
+
+# Figure new 1
+def make_new_figure_1(X):
+    
+    # Init
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fig.suptitle("Baseline")
+
+    # Plot
+    ax.scatter(X[:,0], X[:,1], s=50, alpha=0.5, c='k') 
+    ax.set_xlabel('X0'); ax.set_ylabel('X1');
+    
+
+# Figure new 2
+def make_new_figure_2(X, clusters0):
+    
+    # Init
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fig.suptitle("Random assignment")
+
+    # Plot
+    ax.scatter(X[clusters0==0,0], X[clusters0==0,1], s=50, alpha=0.5) 
+    ax.scatter(X[clusters0==1,0], X[clusters0==1,1], s=50, alpha=0.5)
+    ax.set_xlabel('X0'); ax.set_ylabel('X1');
+
+
+# Plot assignment
+def plot_assignment(X, centroids, clusters, d, i):
+    clear_output(wait=True)
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fig.suptitle("Iteration %.0f: inertia=%.1f" % (i,d))
+
+    # Plot
+    ax.clear()
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color'];
+    K = np.size(centroids,0)
+    for k in range(K):
+        ax.scatter(X[clusters==k,0], X[clusters==k,1], s=50, c=colors[k], alpha=0.5) 
+        ax.scatter(centroids[k,0], centroids[k,1], marker = '*', s=300, color=colors[k])
+        ax.set_xlabel('X0'); ax.set_ylabel('X1');
+    
+    # Show
+    plt.show();
+    
+# Figure new 3
+def make_new_figure_3(d):
+    
+    # Init
+    plt.figure(figsize=(25, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+
+    # calculate full dendrogram
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    d
+    plt.show()
+    
     
 # Figure new 4
 def make_new_figure_4(linkages, titles):
     
     # Init
-    fig, (ax1,ax2,ax3) = plt.subplots(1,3, figsize=(18,10))
+    fig, (ax1,ax2,ax3) = plt.subplots(1,3, figsize=(15,6))
 
     # Plot
     for linkage, t, ax in zip(linkages, titles, (ax1,ax2,ax3)):
         dendrogram(linkage, ax=ax)
         ax.set_title(t)
+        
+        
+def get_cov_ellipse(distr, nstd, **kwargs):
+    """
+    Return a matplotlib Ellipse patch representing the covariance matrix
+    cov centred at centre and scaled by the factor nstd.
+
+    """
+
+    # Find and sort eigenvalues and eigenvectors into descending order
+    eigvals, eigvecs = np.linalg.eigh(distr.cov)
+    order = eigvals.argsort()[::-1]
+    eigvals, eigvecs = eigvals[order], eigvecs[:, order]
+
+    # The anti-clockwise angle to rotate our ellipse by 
+    vx, vy = eigvecs[:,0][0], eigvecs[:,0][1]
+    theta = np.arctan2(vy, vx)
+
+    # Width and height of ellipse to draw
+    width, height = 2 * nstd * np.sqrt(eigvals)
+    return Ellipse(xy=distr.mean, width=width, height=height,
+                   angle=np.degrees(theta), **kwargs)
+
+
+# Plot assignment
+def plot_assignment_gmm(X, clusters, distr, i, logL):
+    clear_output(wait=True)
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fig.suptitle(f"Iteration {i}, logL={logL:.2}")
+
+    # Plot
+    ax.clear()
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color'];
+    K = len(distr)
+    for k in range(K):
+        ax.scatter(X[clusters==k,0], X[clusters==k,1], s=50, c=colors[k], alpha=0.5) 
+        ax.scatter(distr[k].mean[0], distr[k].mean[1], marker = '*', s=300, color=colors[k])
+        for i in [0.5, 1, 2]:
+            ax.add_artist(get_cov_ellipse(distr[k], nstd=i, color=colors[k], alpha=0.05))
+        ax.set_xlabel('X0'); ax.set_ylabel('X1');
+    
+    # Show
+    plt.show();
